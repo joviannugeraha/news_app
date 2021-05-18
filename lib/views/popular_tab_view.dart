84051399685d4b3f7182fb9616f9448fca44cad3
joviannugeraha <joviandmi@gmail.com>
2021-05-19 00:02:59 +0700

    api-services

--- lib/views/popular_tab_view.dart
+++ lib/views/popular_tab_view.dart
@@ -1,79 +1,47 @@
-import 'package:flutter/material.dart';
-import 'package:news_app/constants.dart';
-import 'package:news_app/models/news.dart';
-import 'package:news_app/views/read_news_view.dart';
-import 'package:news_app/widgets/primary_card.dart';
-import 'package:news_app/widgets/secondary_card.dart';
+import 'dart:math';
 
-class PopularTabView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 300.0,
-            padding: EdgeInsets.only(left: 18.0),
-            child: ListView.builder(
-              itemCount: popularList.length,
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                var news = popularList[index];
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ReadNewsView(news: news),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    margin: EdgeInsets.only(right: 12.0),
-                    child: PrimaryCard(news: news),
-                  ),
-                );
-              },
-            ),
-          ),
-          SizedBox(height: 25.0),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: EdgeInsets.only(left: 19.0),
-              child: Text("BASED ON YOUR READING HISTORY",
-                  style: kNonActiveTabStyle),
-            ),
-          ),
-          ListView.builder(
-            itemCount: recentList.length,
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            physics: ScrollPhysics(),
-            itemBuilder: (context, index) {
-              var recent = recentList[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ReadNewsView(news: recent),
-                    ),
-                  );
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 135.0,
-                  margin: EdgeInsets.symmetric(horizontal: 18.0, vertical: 8.0),
-                  child: SecondaryCard(news: recent),
-                ),
-              );
-            },
-          )
-        ],
-      ),
+class News {
+  final String author;
+  final String category;
+  final String content;
+  final String estimate;
+  final String favorite;
+  final String image;
+  final String seen;
+  final String subtitle;
+  final String time;
+  final String title;
+
+  News({
+    this.author,
+    this.category,
+    this.content,
+    this.estimate,
+    this.favorite,
+    this.image,
+    this.seen,
+    this.subtitle,
+    this.time,
+    this.title,
+  });
+
+  factory News.fromJson(Map<String, dynamic> json) {
+    return News(
+      author: json['author'] as String,
+      title: json['title'] as String,
+      subtitle: json['description'] as String,
+      image: json['urlToImage'] as String,
+      content: json['content'] as String,
+      time: json['publishedAt'] as String,
+      category: 'RANDOM',
+      estimate: (Random().nextInt(15) + 1).toString(),
+      favorite: (Random().nextDouble() + Random().nextInt(15) + 1)
+              .toStringAsFixed(1) +
+          'k',
+      seen: (Random().nextInt(100) + 1).toString() + 'k',
     );
   }
 }
+// popularList
+// trendingList
+// recentList
